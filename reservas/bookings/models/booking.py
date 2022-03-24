3ba8187573489304e1ddc1e8c28742ad0dd223be
Luis Alfredo <alf.182.u@gmail.com>
2022-03-24 12:29:09 -0400

    Adicionando metodos de pago

--- reservas/bookings/models/booking.py
+++ reservas/bookings/models/booking.py
@@ -14,12 +14,12 @@ class Booking (BookingAudit):
     check_in_date = models.DateTimeField(blank=False, null=False)
     # CheckOut date
     check_out_date = models.DateTimeField(blank=False, null=False)
-    # Calc Price
-    price = models.DecimalField(max_digits=6, decimal_places=2, default=1)
+    # Calc Amount
+    amount = models.DecimalField(max_digits=6, decimal_places=2, default=1)
     # Payment Method
     payment_method = models.ForeignKey(PaymentMethod, blank=True, null=True, on_delete=models.PROTECT)
     # State of Payment
-    state = models.ForeignKey(State, blank=True, null=True, on_delete=models.PROTECT)
+    state = models.ForeignKey(State, blank=True, null=True, on_delete=models.PROTECT, default='PEN')
     # Customer Data
     customer = models.ForeignKey(Customer, blank=False, null=False, on_delete=models.PROTECT)
     # Room Data
--- reservas/bookings/serializers/booking.py
+++ reservas/bookings/serializers/booking.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, date
 
 # Django Rest Framework
 from rest_framework import serializers
@@ -13,11 +13,14 @@ from reservas.customers.models import Customer
 class BookingSerializer(serializers.Serializer):
     check_in_date = serializers.DateField()
     check_out_date = serializers.DateField()
-    # payment_method = serializers.PrimaryKeyRelatedField(source='paymentmethod.payment_method_id')
-    # state = serializers.PrimaryKeyRelatedField(source='state.state_id')
-
+    payment_method = serializers.PrimaryKeyRelatedField(queryset=PaymentMethod.objects.all(),
+                                                        required=False,
+                                                        allow_null=True)
+    state = serializers.PrimaryKeyRelatedField(queryset=State.objects.all(),
+                                               required=False,
+                                               allow_null=True)
     room = serializers.PrimaryKeyRelatedField(queryset=Room.objects.all())
-
+    # amount = serializers.DecimalField(max_digits=6, decimal_places=2)
     customer = CustomerSerializer(required=True)
 
     def validate(self, data):
@@ -41,17 +44,39 @@ class BookingSerializer(serializers.Serializer):
         is_available = Booking.objects.filter(room=data['room'],
                                               check_in_date__lte=data['check_in_date'],
                                               check_out_date__gte=data['check_out_date']).count()
+
         if is_available > 0:
             raise serializers.ValidationError({
                 'room': 'La habitacion se encuentra ocupada en la fecha seleccionada'
             })
 
+        # Calculamos los dias de estadia
+        delta = data['check_out_date'] - data['check_in_date']
+        number_of_nights = delta.days + 1
+
+        # Calculamos el costo de hospedaje
+        self.context['amount'] = number_of_nights * data['room'].base_price
+
+        # Verificamos el estado enviado que por defecto es PENDIENTE
+        if 'state' not in data:
+            data['state'] = State.objects.get(state_id='PEN')
+        else:
+            # En caso de ser una reserva pagada con anticipacion
+            if data['state'].state_id == 'PAG':
+                # Verificamos el metodo de pago
+                if 'payment_method' not in data:
+                    raise serializers.ValidationError({
+                        'payment_method': 'Debe seleccionar un metodo de pago'
+                    })
+
         return data
 
     def create(self, validated_data):
         customer_data = validated_data.pop('customer')
+        # Creamos al cliente nuevo si no existiese
         obj, created = Customer.objects.get_or_create(
             **customer_data
         )
-        booking_instance = Booking.objects.create(customer=obj, **validated_data)
+        # Creamos la reserva
+        booking_instance = Booking.objects.create(customer=obj, amount=self.context['amount'], **validated_data)
         return booking_instance
